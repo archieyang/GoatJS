acd32fd82e6148167721c39e1aeb1ab99b6769d6
Archie <yangyxcn@gmail.com>
2015-03-20 09:54:53 +0800

    Add store list view
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the goatJsApp
  */
 angular.module('goatJsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.stores = [];
+    $http.get('http://localhost/stores').
+      success(function (data) {
+        for(var i = 0; i < data.length; ++i) {
+          $scope.stores.push(data[i]);
+        }
+      }).
+      error(function(data, status, headers, config) {
+        $scope.error = true;
+      })
   });
